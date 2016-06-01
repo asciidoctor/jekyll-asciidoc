99697e1671003e01768ea918d125273cacbf346b
Dan Allen <dan.j.allen@gmail.com>
2016-06-01 00:21:16 -0600

    qualify classes and modules; update code style

--- lib/jekyll-asciidoc.rb
+++ lib/jekyll-asciidoc.rb
@@ -1,6 +1,6 @@
-JEKYLL_MIN_VERSION_3 = Gem::Version.new(Jekyll::VERSION) >= Gem::Version.new('3.0.0') unless defined? JEKYLL_MIN_VERSION_3
-
 module Jekyll
+  MIN_VERSION_3 = ::Gem::Version.new(VERSION) >= ::Gem::Version.new('3.0.0') unless defined? MIN_VERSION_3
+
   module AsciiDoc
     module Utils
       def self.has_front_matter?(delegate_method, asciidoc_ext_re, path)
@@ -12,15 +12,16 @@ module Jekyll
   module Converters
     class AsciiDocConverter < Converter
       IMPLICIT_ATTRIBUTES = %W(
-        env=site env-site site-gen=jekyll site-gen-jekyll builder=jekyll builder-jekyll jekyll-version=#{Jekyll::VERSION}
+        env=site env-site site-gen=jekyll site-gen-jekyll
+        builder=jekyll builder-jekyll jekyll-version=#{::Jekyll::VERSION}
       )
       HEADER_BOUNDARY_RE = /(?<=\p{Graph})\n\n/
       STANDALONE_HEADER = %([%standalone]\n)
 
       safe true
 
-      highlighter_prefix "\n"
-      highlighter_suffix "\n"
+      highlighter_prefix %(\n)
+      highlighter_suffix %(\n)
 
       def initialize(config)
         @config = config
@@ -38,7 +39,7 @@ module Jekyll
             attributes.unshift('notitle', 'hardbreaks', 'idprefix', 'idseparator=-', 'linkattrs')
             attributes.concat(IMPLICIT_ATTRIBUTES)
           end
-          if JEKYLL_MIN_VERSION_3 && !config['asciidoc_require_front_matter']
+          if ::Jekyll::MIN_VERSION_3 && !config['asciidoc_require_front_matter']
             if (del_method = ::Jekyll::Utils.method(:has_yaml_header?))
               unless (new_method = ::Jekyll::AsciiDoc::Utils.method(:has_front_matter?)).respond_to?(:curry)
                 new_method = new_method.to_proc # Ruby < 2.2
@@ -57,15 +58,15 @@ module Jekyll
         when 'asciidoctor'
           begin
             require 'asciidoctor' unless defined? ::Asciidoctor
-          rescue LoadError
+          rescue ::LoadError
             STDERR.puts 'You are missing a library required to convert AsciiDoc files. Please run:'
             STDERR.puts '  $ [sudo] gem install asciidoctor'
-            raise FatalException.new('Missing dependency: asciidoctor')
+            raise ::FatalException.new('Missing dependency: asciidoctor')
           end
         else
-          STDERR.puts "Invalid AsciiDoc processor: #{@config['asciidoc']}"
+          STDERR.puts %(Invalid AsciiDoc processor: #{@config['asciidoc']})
           STDERR.puts '  Valid options are [ asciidoctor ]'
-          raise FatalException.new("Invalid AsciiDoc processor: #{@config['asciidoc']}")
+          raise ::FatalException.new(%(Invalid AsciiDoc processor: #{@config['asciidoc']}))
         end
       end
 
@@ -85,7 +86,7 @@ module Jekyll
         end
         case @config['asciidoc']
         when 'asciidoctor'
-          Asciidoctor.convert(content, @config['asciidoctor'].merge(header_footer: standalone))
+          ::Asciidoctor.convert(content, @config['asciidoctor'].merge(header_footer: standalone))
         else
           warn 'Unknown AsciiDoc converter. Passing through unparsed content.'
           content
@@ -99,7 +100,7 @@ module Jekyll
         case @config['asciidoc']
         when 'asciidoctor'
           # NOTE return a document even if header is empty because attributes may be inherited from config
-          Asciidoctor.load(header, @config['asciidoctor'].merge(parse_header_only: true))
+          ::Asciidoctor.load(header, @config['asciidoctor'].merge(parse_header_only: true))
         else
           warn 'Unknown AsciiDoc converter. Cannot load document header.'
         end
@@ -116,13 +117,13 @@ module Jekyll
         end
       end
 
-      STANDALONE_HEADER = Converters::AsciiDocConverter::STANDALONE_HEADER
+      STANDALONE_HEADER = ::Jekyll::Converters::AsciiDocConverter::STANDALONE_HEADER
       AUTO_PAGE_LAYOUT_LINE = %(:page-layout: _auto\n)
 
       def generate(site)
-        asciidoc_converter = JEKYLL_MIN_VERSION_3 ?
-            site.find_converter_instance(Jekyll::Converters::AsciiDocConverter) :
-            site.getConverterImpl(Jekyll::Converters::AsciiDocConverter)
+        asciidoc_converter = ::Jekyll::MIN_VERSION_3 ?
+            site.find_converter_instance(::Jekyll::Converters::AsciiDocConverter) :
+            site.getConverterImpl(::Jekyll::Converters::AsciiDocConverter)
         asciidoc_converter.setup
         unless (page_attr_prefix = site.config['asciidoc_page_attribute_prefix']).empty?
           page_attr_prefix = %(#{page_attr_prefix}-)
@@ -140,12 +141,12 @@ module Jekyll
             unless (adoc_front_matter = doc.attributes
                 .select {|name| name.start_with?(page_attr_prefix) }
                 .map {|name, val| %(#{name[page_attr_prefix_l..-1]}: #{val == '' ? '""' : val}) }).empty?
-              page.data.update(SafeYAML.load(adoc_front_matter * "\n"))
+              page.data.update(::SafeYAML.load(adoc_front_matter * %(\n)))
             end
 
             case page.data['layout']
             when nil
-              page.content = STANDALONE_HEADER + page.content unless page.data.key? 'layout'
+              page.content = STANDALONE_HEADER + page.content unless page.data.key?('layout')
             when '', '_auto'
               page.data['layout'] = 'default'
             when false
@@ -157,24 +158,24 @@ module Jekyll
           end
         end
 
-        (JEKYLL_MIN_VERSION_3 ? site.posts.docs : site.posts).each do |post|
-          if asciidoc_converter.matches(JEKYLL_MIN_VERSION_3 ? post.data['ext'] : post.ext)
+        (::Jekyll::MIN_VERSION_3 ? site.posts.docs : site.posts).each do |post|
+          if asciidoc_converter.matches(::Jekyll::MIN_VERSION_3 ? post.data['ext'] : post.ext)
             preamble = post.data.key?('layout') ? '' : AUTO_PAGE_LAYOUT_LINE
             next unless (doc = asciidoc_converter.load_header(preamble + post.content))
 
             post.data['title'] = doc.doctitle if doc.header?
             post.data['author'] = doc.author if doc.author
-            post.data['date'] = DateTime.parse(doc.revdate).to_time if doc.attr? 'revdate'
+            post.data['date'] = ::DateTime.parse(doc.revdate).to_time if doc.attr? 'revdate'
 
             unless (adoc_front_matter = doc.attributes
                 .select {|name| name.start_with?(page_attr_prefix) }
                 .map {|name, val| %(#{name[page_attr_prefix_l..-1]}: #{val == '' ? '""' : val}) }).empty?
-              post.data.update(SafeYAML.load(adoc_front_matter * "\n"))
+              post.data.update(::SafeYAML.load(adoc_front_matter * %(\n)))
             end
 
             case post.data['layout']
             when nil
-              post.content = STANDALONE_HEADER + post.content unless post.data.key? 'layout'
+              post.content = STANDALONE_HEADER + post.content unless post.data.key?('layout')
             when '', '_auto'
               post.data['layout'] = 'post'
             when false
@@ -197,9 +198,9 @@ module Jekyll
     # Returns the HTML formatted String.
     def asciidocify(input)
       site = @context.registers[:site]
-      converter = JEKYLL_MIN_VERSION_3 ?
-          site.find_converter_instance(Jekyll::Converters::AsciiDocConverter) :
-          site.getConverterImpl(Jekyll::Converters::AsciiDocConverter)
+      converter = ::Jekyll::MIN_VERSION_3 ?
+          site.find_converter_instance(::Jekyll::Converters::AsciiDocConverter) :
+          site.getConverterImpl(::Jekyll::Converters::AsciiDocConverter)
       converter.convert(input)
     end
   end
