c31a57d36cab0b7f1ca9062984379492072ce0f2
Dan Allen <dan.j.allen@gmail.com>
2016-06-14 23:55:00 -0600

    minor code style change

--- lib/jekyll-asciidoc.rb
+++ lib/jekyll-asciidoc.rb
@@ -5,12 +5,11 @@ module Jekyll
     module Configured; end
     module Utils
       extend self
-      def has_front_matter?(delegate_method, asciidoc_ext_re, path)
-        ::File.extname(path) =~ asciidoc_ext_re ? true : delegate_method.call(path)
+      def has_front_matter?(dlg_method, asciidoc_ext_re, path)
+        ::File.extname(path) =~ asciidoc_ext_re ? true : dlg_method.call(path)
       end
 
-      begin
-        # supports Jekyll >= 2.3.0
+      begin # supports Jekyll >= 2.3.0
         define_method(:has_yaml_header?, &::Jekyll::Utils.method(:has_yaml_header?))
       rescue ::NameError; end
     end
