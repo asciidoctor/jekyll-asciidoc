612426420f688450c04f13fda4defaf062e45bff
Dan Allen <dan.j.allen@gmail.com>
2016-06-29 23:58:43 -0600

    parse page attribute values individually (PR #115)

--- lib/jekyll-asciidoc/utils.rb
+++ lib/jekyll-asciidoc/utils.rb
@@ -52,16 +52,28 @@ module Jekyll
         site.find_converter_instance Converter
       end
 
-      def prepare_yaml_value val
-        return val unless ::String === val
+      # Parse the specified value as though it is a single-line value part of a
+      # YAML key/value pair.
+      #
+      # Attempt to parse the specified String value as though it is a
+      # single-line value part of a YAML key/value pair. If the value fails to
+      # parse, wrap the value in single quotes (after escaping any single
+      # quotes in the value) and parse it as a character sequence. If the value
+      # is empty, return an empty String.
+      #
+      # val - The String value to parse.
+      #
+      # Returns an [Object] parsed from the string-based YAML value or empty
+      # [String] if the specified value is empty.
+      def parse_yaml_value val
         if val.empty?
-          '\'\''
+          ''
         else
           begin
             ::SafeYAML.load %(--- #{val})
-            val
           rescue
-            (val.include? '\'') ? %(\'#{val.gsub '\'', '\'\''}\') : %(\'#{val}\')
+            val = val.gsub '\'', '\'\'' if val.include? '\''
+            ::SafeYAML.load %(--- \'#{val}\')
           end
         end
       end
