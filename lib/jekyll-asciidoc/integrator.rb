8537b89b04d9a6b93b0fd69ce08013a335db14e4
Dan Allen <dan.j.allen@gmail.com>
2016-07-23 23:35:23 -0600

    return false explicitly when header can't be parsed

--- lib/jekyll-asciidoc/integrator.rb
+++ lib/jekyll-asciidoc/integrator.rb
@@ -57,7 +57,7 @@ module Jekyll
       def integrate document, collection_name = nil
         document.extend Document
         document.content = [%(:#{@page_attr_prefix}layout: _auto), document.content] * NewLine unless document.data.key? 'layout'
-        return unless (doc = @converter.load_header document)
+        return false unless (doc = @converter.load_header document)
 
         document.data['title'] = doc.doctitle if doc.header?
         document.data['author'] = doc.author if doc.author
