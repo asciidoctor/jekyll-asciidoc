ddbcdf330ba761591373718246d3d5cc82ec31cf
Dan Allen <dan.j.allen@gmail.com>
2019-06-03 01:47:26 -0600

    fix warnings

--- lib/jekyll-asciidoc/jekyll_ext/drops/drop.rb
+++ lib/jekyll-asciidoc/jekyll_ext/drops/drop.rb
@@ -1,10 +1,14 @@
-class Jekyll::Drops::Drop
-  class << self
-    # NOTE fixes "warning: instance variable @is_mutable not initialized"
-    prepend (Module.new do
-      def mutable?
-        @is_mutable ||= nil
+module Jekyll
+  module Drops
+    class Drop
+      class << self
+        # NOTE fixes "warning: instance variable @is_mutable not initialized"
+        prepend (Module.new do
+          def mutable?
+            @is_mutable ||= nil # rubocop:disable Naming/MemoizedInstanceVariableName
+          end
+        end)
       end
-    end)
+    end
   end
 end if defined? Jekyll::Drops::Drop
